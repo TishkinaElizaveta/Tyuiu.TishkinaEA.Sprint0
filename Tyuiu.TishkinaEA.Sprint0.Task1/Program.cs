ae298deed3f7006c54b71033075f95e66c1129ce
tishkinaea@outlook.com <tishkinaea@outlook.com>
2024-10-02 17:59:59 +0300

    Task1

---
+++ Tyuiu.TishkinaEA.Sprint0.Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint0.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string name = "Лиза";
+            int age = 18;
+
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
