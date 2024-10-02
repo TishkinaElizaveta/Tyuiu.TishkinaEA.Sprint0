b8411b0e4497055d9eed2a64a4ad9fe2b5cd6bf7
Пользователь <@DESKTOP-T8HLSM3>
2024-10-02 17:45:11 +0300

    Add project files.

---
+++ Tyuiu.TishkinaEA.sprint0.task0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.sprint0.task0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
