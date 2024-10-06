795453d997e44577c4debe2c82155800af0f7126
tishkinaea@outlook.com <tishkinaea@outlook.com>
2024-10-06 14:35:05 +0300

    Task6

---
+++ Tyuiu.TishkinaEA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TishkinaEA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.TishkinaEA.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма эллементов массива = " + DataService.AdditionArray(arraynums));
+            
+            Console.WriteLine("Разность эллементов массива = " + DataService.SubstractionArray(arraynums));
+            
+            Console.WriteLine("Произведение эллементов массива = " + DataService.MultiplicationArray(arraynums));
+            
+            Console.ReadKey();
+        }
+    }
+}
